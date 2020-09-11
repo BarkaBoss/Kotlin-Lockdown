47504d414fa8d7e7149ef480d8cd7fc454be9531
Barka Fori <bossmajor.biii@gmail.com>
2020-09-11 19:31:55 +0100

    Base Keyboard

---
+++ CustomKeyboard/app/src/main/java/xyz/nokt/btf/customkeyboard/NoktKeyboard.java
@@ -0,0 +1,88 @@
+package xyz.nokt.btf.customkeyboard;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.os.IBinder;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class NoktKeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView KBv;
+    private Keyboard keyboard;
+
+    private boolean isCaps = false;
+
+    @Override
+    public View onCreateInputView() {
+        KBv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
+        keyboard = new Keyboard(this, R.xml.qwerty);
+        KBv.setKeyboard(keyboard);
+        KBv.setOnKeyboardActionListener(this);
+        return KBv;
+    }
+
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+
+        switch (primaryCode){
+            case Keyboard.KEYCODE_DELETE:
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                isCaps = !isCaps;
+                keyboard.setShifted(isCaps);
+                KBv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char)primaryCode;
+                if(Character.isLetter(code) && isCaps)
+                    code = Character.toUpperCase(code);
+                ic.commitText(String.valueOf(code), primaryCode);
+        }
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
